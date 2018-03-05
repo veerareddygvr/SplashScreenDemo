a10589dcabf55c43bb7d4b77757b955e7a731bf4
Vera <srininet@srinis-macbook-air-4.local>
2018-03-05 12:12:02 +0530

    Initial Commit

---
+++ SplashScreenDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SplashScreenDemo
+//
+//  Created by Vera on 14/12/1939 Saka.
+//  Copyright © 1939 XeperTechnologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("background color changed")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
