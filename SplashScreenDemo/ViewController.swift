e1ba6b43ced128bdd98b3a4c89ae3156b8119e21
Vera <srininet@srinis-macbook-air-4.local>
2018-03-05 12:18:49 +0530

    removed print

--- SplashScreenDemo/ViewController.swift
+++ SplashScreenDemo/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        self.view.backgroundColor = UIColor.blue
-        print(self.view.frame)
+        
     }
 
     override func didReceiveMemoryWarning() {
